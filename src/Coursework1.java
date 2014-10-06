1a235f35e533a1d09deceafae79c860bf8fda2b0
= <=>
2014-10-06 12:43:03 +0100

    empty array checking and messages. arraysEmpty method

--- src/Coursework1.java
+++ src/Coursework1.java
@@ -21,17 +21,23 @@ public class Coursework1 {
     public static void main(String[] args) {
         int[] array1 = fillArray(ARRAYLENGTH);
         int[] array2 = fillArray(ARRAYLENGTH);
-        int[] commonData = compareCommon(array1, array2);
-        int[] uniqueData1 = compareUnique(array1, array2);
-        int[] uniqueData2 = compareUnique(array2, array1);
 
+        if (!arraysEmpty(array1, array2)) {
+            int[] commonData = compareCommon(array1, array2);
+            int[] uniqueData1 = compareUnique(array1, array2);
+            int[] uniqueData2 = compareUnique(array2, array1);
 
-        printResultArray("Array 1 values: ", array1);
-        printResultArray("Array 2 values: ", array2);
-        printResultArray("Common data: ", commonData);
-        System.out.println("Number of common data: " + commonData.length);
-        printResultArray("Unique values for array 1: ", uniqueData1);
-        printResultArray("Unique values for array 2: ", uniqueData2);
+            printResultArray("Array 1 values: ", array1);
+            printResultArray("Array 2 values: ", array2);
+            printResultArray("Common data: ", commonData);
+            if (commonData.length != 0) {
+                System.out.println("Number of common data: " + commonData.length);
+            }
+            printResultArray("Unique values for array 1: ", uniqueData1);
+            printResultArray("Unique values for array 2: ", uniqueData2);
+
+            System.out.println();
+        }
 ;
 
     }
@@ -117,13 +123,18 @@ public class Coursework1 {
      * @param array integer array to convert to String
      */
     private static void printResultArray(String description, int[] array) {
-        String resultString = "";
+        if(array == null || array.length == 0 ) {
+            System.out.println(description + "none");
+        } else {
 
-        for(int element : array){
-            resultString += element + " ";
+            String resultString = "";
+
+            for (int element : array) {
+                resultString += element + " ";
+            }
+
+            System.out.println(description + resultString);
         }
-
-        System.out.println(description + resultString);
     }
 
 
@@ -139,9 +150,11 @@ public class Coursework1 {
 
         if(array1.length >= array2.length){
             arrayLength = array1.length;
+
         } else arrayLength = array2.length;
 
         int[] maxArray = new int[arrayLength];
+
         for(int i : array1){
             for(int j : array2){
                 if(i == j){
@@ -168,34 +181,61 @@ public class Coursework1 {
      * @return unique array of unique integer elements in first array
      */
     private static int[] compareUnique(int[] arrayToPrintUnique, int[] arrayToCompare) {
-        int[] common = compareCommon(arrayToPrintUnique, arrayToCompare); //overload method for optional common param?
-        int arrayLength = (arrayToPrintUnique.length) - (common.length);
-        int[] unique = new int[arrayLength];
-        boolean uniqueEl = true;
-        int k = 0;
+        int[] unique;
 
-        for(int i : arrayToPrintUnique){
-            for(int j : common){
-                if(i == j) {
-                    uniqueEl = false;
-                    break;
-                }
-                if (uniqueEl) {
-                    if (!dupeInput(i, unique)) {
-                        if (k < unique.length) {
-                            unique[k] = i;
-                            k++;
+        if (arrayToCompare.length == 0) {
+            unique = arrayToPrintUnique;
+
+        } else if (arrayToPrintUnique.length == 0) {
+            unique = null;
+        } else {
+            int[] common = compareCommon(arrayToPrintUnique, arrayToCompare); //overload method for optional common param?
+            int arrayLength = (arrayToPrintUnique.length) - (common.length);
+            int[] maxArray = new int[arrayLength];
+            boolean uniqueEl = true;
+            int k = 0;
+
+            for (int i : arrayToPrintUnique) {
+                for (int j : common) {
+                    if (i == j) {
+                        uniqueEl = false;
+                        break;
+                    }
+                    if (uniqueEl) {
+                        if (!dupeInput(i, maxArray)) {
+                            if (k < maxArray.length) {
+                                maxArray[k] = i;
+                                k++;
+                            }
                         }
                     }
+                    uniqueEl = true;
                 }
-                uniqueEl = true;
+
             }
 
+            unique = fitArray(maxArray);
         }
+
         return unique;
 
     }
 
 
+    /**
+     * arraysEmpty method check two arrays and prints message if both are null or empty
+     * @param array1 one of two integer arrays to check
+     * @param array2 one of two integer arrays to check
+     * @return boolean value indicating both arrays are empty/null
+     */
+    private static boolean arraysEmpty(int[] array1, int[] array2) {
+        boolean empty = false;
+         if ((array1 == null && array2 == null) || (array1.length == 0 && array2.length == 0)) {
+             empty = true;
+             System.out.println("Both arrays are empty.");
+         }
+
+        return empty;
+    }
 
 }
